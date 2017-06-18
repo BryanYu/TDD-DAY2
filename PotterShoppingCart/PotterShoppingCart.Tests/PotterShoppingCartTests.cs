28715d13e2c6c7fb17b40dae697fcaa8e9e26ebe
BryanYu <d58526@gmail.com>
2017-06-18 08:04:23 -0700

    第一個測試案例(紅燈)

---
+++ PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -0,0 +1,35 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class PotterShoppingCartTests
+    {
+        private List<Book> _dummyBooks = new List<Book>
+        {
+            new Book { Name = "哈利波特第一集",Price = 100 },
+            new Book { Name = "哈利波特第一集",Price = 100 },
+            new Book { Name = "哈利波特第一集",Price = 100 },
+            new Book { Name = "哈利波特第一集",Price = 100 },
+            new Book { Name = "哈利波特第一集",Price = 100 }
+        };
+
+        [TestMethod]
+        public void Test_Every_PotterBooks_Price_is_100()
+        {
+            ///Arrange
+            var target = new PotterBooks();
+            var expected = _dummyBooks;
+            ///Act
+            List<Book> actual = target.Get();
+            ///Assert
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+    }
+}
---
+++ PotterShoppingCart/PotterShoppingCart/PotterBooks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using PotterShoppingCart.Model;
+
+namespace PotterShoppingCart
+{
+    public class PotterBooks
+    {
+        public PotterBooks()
+        {
+        }
+
+        public List<Book> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
